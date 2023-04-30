3f6c85e03d96cbaf9683a1bb8431124cb6eee4fb
Akshadachobhee <akshadachobhe7@gmail.com>
2023-04-30 23:26:02 +0530

    Forms

---
+++ Forms/1/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+// import classes which are required for reactive forms
+import {FormGroup,FormControl} from '@angular/forms'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent 
+{
+  MarvellousForm = new FormGroup(
+    {
+      username : new FormControl('Piyush'),
+      passowrd : new FormControl(''),
+      ConfirmPass : new FormControl('')
+
+    }
+  );
+}
